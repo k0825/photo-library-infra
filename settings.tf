54c92d0f22cb884089ef0eb2230d5b6ed404df7f
k0825 <kazukkiyo@icloud.com>
2023-06-10 22:25:56 +0900

    init

---
+++ settings.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ikari-tfstate"
+    key    = "photo-library-infra/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
