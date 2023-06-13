5622bbdc6f76346b1d6baba85c57440af03e3f1d
k0825 <kazukkiyo@icloud.com>
2023-06-14 02:05:27 +0900

    デッドレターキューを作成

--- modules/create_thumbnail/sqs.tf
+++ modules/create_thumbnail/sqs.tf
@@ -1,10 +1,14 @@
 resource "aws_sqs_queue" "queue" {
-  name                       = "create_thumbnail_queue"
+  name                       = local.create_thumbnail_queue
   message_retention_seconds  = 60 * 60 * 24
   visibility_timeout_seconds = 900 + 60
   delay_seconds              = 0
   receive_wait_time_seconds  = 0
   max_message_size           = 262144
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dead_letter_queue.arn,
+    maxReceiveCount     = 3
+  })
 }
 
 resource "aws_sqs_queue_policy" "queue_policy" {
@@ -14,4 +18,13 @@ resource "aws_sqs_queue_policy" "queue_policy" {
     sqs_queue_name = aws_sqs_queue.queue.name
     bucket_name    = var.photo_library_name
   })
+}
+
+resource "aws_sqs_queue" "dead_letter_queue" {
+  name = "${local.create_thumbnail_queue}-dlq"
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = ["arn:aws:sqs:ap-northeast-1:${local.account_id}:${local.create_thumbnail_queue}"]
+  })
+
 }
