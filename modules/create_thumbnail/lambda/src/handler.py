30c0f0bbc8c0a538ae65ebb6d3bb4667ea965673
k0825 <kazukkiyo@icloud.com>
2023-06-14 18:04:05 +0900

    SQS用にイベントを修正

--- modules/create_thumbnail/lambda/src/handler.py
+++ modules/create_thumbnail/lambda/src/handler.py
@@ -61,59 +61,58 @@ def move_to_other_object(bucket, original_path):
 
 
 def lambda_handler(event, context):
-    logger.info(f"{len(event)}個のイベントを受け取りました")
-
     pattern = ".\.(jpg|jpeg|png|heic|gif)$"
-    
-    sqs_body = json.loads(event['body'])
+    for sqs_record in event["Records"]:
+        sqs_body = json.loads(sqs_record["body"])
 
-    # S3のイベントからバケット名とオリジナル画像のパスを取得
-    for record in sqs_body["Records"]:
-        bucket = record["s3"]["bucket"]["name"]
-        original_path = record["s3"]["object"]["key"]
+        # S3のイベントからバケット名とオリジナル画像のパスを取得
+        for record in sqs_body["Records"]:
+            logger.info(f"{len(event)}個のS3イベントを受け取りました")
+            bucket = record["s3"]["bucket"]["name"]
+            original_path = record["s3"]["object"]["key"]
 
-        logger.info(f"ファイルパス: {original_path}")
+            logger.info(f"ファイルパス: {original_path}")
 
-        if not re.search(pattern, original_path, re.IGNORECASE):
+            if not re.search(pattern, original_path, re.IGNORECASE):
+                try:
+                    move_to_other_object(bucket, original_path)
+                except:
+                    logger.error("ファイルの移動に失敗しました")
+                    logger.error(e)
+                    return
+
+                logger.info("この拡張子は対象外のため、ファイルを移動しました")
+                continue
+
+            # オリジナル画像を取得
             try:
-                move_to_other_object(bucket, original_path)
-            except:
-                logger.error("ファイルの移動に失敗しました")
+                image = get_original_image(bucket, original_path)
+            except Exception as e:
+                logger.error("オリジナル画像の取得に失敗しました")
                 logger.error(e)
                 return
 
-            logger.info("この拡張子は対象外のため、ファイルを移動しました")
-            continue
 
-        # オリジナル画像を取得
-        try:
-            image = get_original_image(bucket, original_path)
-        except Exception as e:
-            logger.error("オリジナル画像の取得に失敗しました")
-            logger.error(e)
-            return
-
-
-        # サムネイル画像を作成
-        try:
-            uid = hashlib.sha256(original_path.encode()).hexdigest()
-            thumbnail_path = create_thumbnail(image, bucket, uid)
-        except Exception as e:
-            logger.error("サムネイル画像の作成に失敗しました")
-            logger.error(e)
-            return
+            # サムネイル画像を作成
+            try:
+                uid = hashlib.sha256(original_path.encode()).hexdigest()
+                thumbnail_path = create_thumbnail(image, bucket, uid)
+            except Exception as e:
+                logger.error("サムネイル画像の作成に失敗しました")
+                logger.error(e)
+                return
 
 
-        # DynamoDBに登録
-        try:
-            table_name = os.environ["MAPPING_TABLE_NAME"]
-            item = {
-                "id": {"S": uid},
-                "original_path": {"S": original_path},
-                "thumbnail_path": {"S": thumbnail_path},
-            }
-            dynamodb.put_item(TableName=table_name, Item=item)
-        except:
-            logger.error("DynamoDBへの登録に失敗しました")
-            logger.error(e)
-            return
+            # DynamoDBに登録
+            try:
+                table_name = os.environ["MAPPING_TABLE_NAME"]
+                item = {
+                    "id": {"S": uid},
+                    "original_path": {"S": original_path},
+                    "thumbnail_path": {"S": thumbnail_path},
+                }
+                dynamodb.put_item(TableName=table_name, Item=item)
+            except:
+                logger.error("DynamoDBへの登録に失敗しました")
+                logger.error(e)
+                return
